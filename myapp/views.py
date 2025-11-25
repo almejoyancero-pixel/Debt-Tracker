f770b4323eed9532db2683531f15514c2bde83fa
alme <almejoyancero@gmail.com>
2025-11-25 22:14:15 +0800

    me

--- myapp/views.py
+++ myapp/views.py
@@ -620,8 +620,7 @@ def creditor_delete_debt(request, id):
         
         # Preserve payment records by setting debt reference to None before deletion
         payments = Payment.objects.filter(debt=debt)
-        payments.update(debt=None, 
-                       description=f"Payment for deleted debt: {debtor.full_name} - ₱{debt_amount}")
+        payments.update(debt=None)
         
         # Delete the debt permanently
         debt.delete()
